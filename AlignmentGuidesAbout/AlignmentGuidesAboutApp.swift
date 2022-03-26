549b89218f266f921ce95e9d12f99bd5edafbbfc
张亚飞 <672233580@qq.com>
2022-03-26 20:24:53 +0800

    Initial Commit

---
+++ AlignmentGuidesAbout/AlignmentGuidesAboutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignmentGuidesAboutApp.swift
+//  AlignmentGuidesAbout
+//
+//  Created by 亚飞 on 2022/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AlignmentGuidesAboutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
